37cf43d39a2555d0483584dc2e8851e5efd6f0f7
Joseph Daniels <joedaniels29@me.com>
2016-10-07 21:26:03 -0400

    Initial commit

---
+++ Chronolink/Chronolink.h
@@ -0,0 +1,19 @@
+//
+//  Chronolink.h
+//  Chronolink
+//
+//  Created by Joseph Daniels on 07/10/16.
+//  Copyright © 2016 Joseph Daniels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chronolink.
+FOUNDATION_EXPORT double ChronolinkVersionNumber;
+
+//! Project version string for Chronolink.
+FOUNDATION_EXPORT const unsigned char ChronolinkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chronolink/PublicHeader.h>
+
+
